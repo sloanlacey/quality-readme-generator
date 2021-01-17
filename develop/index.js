85f85cbf2aa589d9ab3c3604923671e8df593263
sloanlacey <sloanlacey89@gmail.com>
2021-01-16 17:29:46 -0800

    Install path npm.

--- develop/index.js
+++ develop/index.js
@@ -1,7 +1,24 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+// File system interaction
+const fs = require('fs');
+// Path
+const path = require('path');
+// Reference to generate.js
+// const generate = require('./utils/generate');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }, {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
